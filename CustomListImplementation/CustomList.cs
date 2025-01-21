247817ae2d6ed1d4eae2a3f00e70f391d046c67a
Rahul Gupta-Gunners <rahulbcsit@hotmail.com>
2025-01-20 23:10:55 -0600

    add to the file

--- CustomListImplementation/CustomList.cs
+++ CustomListImplementation/CustomList.cs
@@ -1,13 +1,65 @@
 
+using System.Collections; 
+
 public interface ICustomList<T> : IEnumerable<T>
 {
       void Add(T item);
       bool Remove(T item);
 
-      void RemoveAt(T item);
+      void RemoveAt(int item);
 
       T this[int index] {get; set;}
 
       int Count {get;} 
-}
+} 
 
+public class CustomList<T> : ICustomList<T>
+{
+      private readonly  List<T> _items;
+
+      public CustomList()
+      {
+            _items = new List<T>();
+      } 
+
+      public int Count => _items.Count;
+
+      public T this[int index]
+      {
+            get { return _items[index];}
+            set 
+            {
+                  _items[index] = value;
+            } 
+      }
+
+      public void Add(T item)
+      {
+            _items.Add(item);
+      }
+
+      public bool Remove(T item)
+      {
+            return _items.Remove(item);
+      }
+
+      public void RemoveAt(int index)
+      {
+            if (index < 0 || index >= _items.Count)
+            {
+                  throw new ArgumentOutOfRangeException(nameof(index), $"{index} is out of range");
+            }
+
+            _items.RemoveAt(index);
+      }
+      
+      public IEnumerator<T> GetEnumerator()
+      {
+             foreach (var item in _items)
+             {
+                  yield return item;
+             }
+      }
+
+      IEnumerator IEnumerable.GetEnumerator() => GetEnumerator(); 
+}
